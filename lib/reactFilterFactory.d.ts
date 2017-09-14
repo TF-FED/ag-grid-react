8a8fda4241d7ff038ab71c96b61bca3735bab57b
Fuyun <ifuyun@hotmail.com>
2017-09-14 17:54:55 +0800

    init fork.

--- lib/reactFilterFactory.d.ts
+++ lib/reactFilterFactory.d.ts
@@ -1,5 +1,5 @@
 // ag-grid-react v13.1.0
-import { IFilterComp } from "ag-grid";
+import { IFilterComp } from "tf-ag-grid";
 export declare function reactFilterFactory(reactComponent: any, parentComponent?: any): {
     new (): IFilterComp;
 };
